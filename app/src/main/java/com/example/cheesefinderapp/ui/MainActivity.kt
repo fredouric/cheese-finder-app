b7c9a82082ae1ce5a1ac6036204ab17e2de4e07f
Frédéric Thomas <frederic.thomas2@icloud.com>
2023-11-28 14:53:50 +0100

    feat(ui): styled navigation bar and added "about" fragment

--- app/src/main/java/com/example/cheesefinderapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/cheesefinderapp/ui/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.cheesefinderapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.cheesefinderapp.data.services.CheeseService
+import androidx.fragment.app.Fragment
 import com.example.cheesefinderapp.R
+import com.example.cheesefinderapp.data.services.CheeseService
 import com.example.cheesefinderapp.model.Cheese
 import com.example.cheesefinderapp.model.CheeseCollection
+import com.example.cheesefinderapp.ui.about.AboutFragment
 import com.example.cheesefinderapp.ui.cheeselist.CheeseListFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -14,6 +17,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 const val SERVER_BASE_URL = "https://cheese-epg-fts.cleverapps.io"
+
 class MainActivity : AppCompatActivity() {
 
     private val cheeseCollection = CheeseCollection()
@@ -32,21 +36,48 @@ class MainActivity : AppCompatActivity() {
                 override fun onResponse(
                     call: Call<List<Cheese>>,
                     response: Response<List<Cheese>>
-                ){
+                ) {
                     response.body()?.forEach {
                         val cheese = Cheese(it.departement, it.fromage, it.lait, it.geo_point_2d)
                         cheeseCollection.addCheese(cheese)
                     }
-                    val cheeseFragment = CheeseListFragment.newInstance(cheeseCollection.getArrayList())
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.am_fragment_accueil, cheeseFragment)
-                        .commit()
+                    loadFragment(CheeseListFragment.newInstance(cheeseCollection.getArrayList()))
                 }
 
                 override fun onFailure(call: Call<List<Cheese>>, t: Throwable) {
-                    // DO THINGS
                     t.printStackTrace()
                 }
-    })
+            })
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.am_navigation_bar)
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.a_menu_item_home -> {
+                    loadFragment(CheeseListFragment.newInstance(cheeseCollection.getArrayList()))
+                    true
+                }
+
+                R.id.a_menu_item_map -> {
+                    // Handle map item click if needed
+                    true
+                }
+
+                R.id.a_menu_item_about -> {
+                    loadFragment(AboutFragment.newInstance())
+                    true
+                }
+
+                else -> false
+            }
+
+        }
     }
+
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.am_fragment_accueil, fragment)
+            .commit()
+    }
+
 }
