e65c37c0474a4610a70fd33c31743e204260ab94
Frédéric Thomas <frederic.thomas2@icloud.com>
2023-12-09 15:11:06 +0100

    feat(mapsFragment): marker for each department + geo_shape

--- app/src/main/java/com/example/cheesefinderapp/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/example/cheesefinderapp/ui/maps/MapsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.cheesefinderapp.ui.maps
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +14,9 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.transition.MaterialFadeThrough
+import com.google.gson.Gson
+import com.google.maps.android.data.geojson.GeoJsonLayer
+import org.json.JSONObject
 
 
 private const val ARG_CHEESE_COLLECTION = "param_cheese_collection"
@@ -23,13 +27,43 @@ class MapsFragment : Fragment() {
 
     private val callback = OnMapReadyCallback { googleMap ->
 
+        val departmentLayers = HashMap<String, GeoJsonLayer>()
+        val departmentMarkerCoordinates = HashMap<String, LatLng>()
+        val gson = Gson()
+
         cheeseCollection?.forEach {
-            val coordinates = LatLng(it.geo_point_2d.lat, it.geo_point_2d.lon)
-            googleMap.addMarker(MarkerOptions().position(coordinates).title(it.fromage))
+            val jsonString = gson.toJson(it.geo_shape)
+            val layer = GeoJsonLayer(googleMap, JSONObject(jsonString))
+            departmentLayers[it.departement] = layer
+            departmentMarkerCoordinates[it.departement] =
+                LatLng(it.geo_point_2d.lat, it.geo_point_2d.lon)
         }
+
+        departmentLayers.forEach { (departmentName, layer) ->
+            departmentMarkerCoordinates[departmentName]?.let {
+                MarkerOptions().position(it)
+                    .title(departmentName)
+                    .snippet(showCheesesForDepartment(departmentName))
+            }?.let {
+                googleMap.addMarker(
+                    it
+                )
+            }
+
+            val layerStyle = layer.defaultPolygonStyle
+            layerStyle.strokeColor = Color.RED
+            layerStyle.strokeWidth = 2.5f
+            layer.addLayerToMap()
+        }
+
         val centreFrance = LatLng(46.606111, 1.875278)
         googleMap.moveCamera(CameraUpdateFactory.zoomBy(3.25f))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(centreFrance))
+    }
+
+    private fun showCheesesForDepartment(departmentName: String): String? {
+        val cheesesInDepartment = cheeseCollection?.filter { it.departement == departmentName }
+        return cheesesInDepartment?.joinToString(", ") { it.fromage }
     }
 
     override fun onCreateView(
